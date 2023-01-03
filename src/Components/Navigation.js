f6c401f2c9c2c0398ca3124473dbb1e45767a126
Lintang Aji Yoga Pratama <lintangajiyogapratama@gmail.com>
2023-01-03 21:24:53 +0700

    feat: adding a transition to the navbar

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,37 +1,79 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useTransition, animated } from "@react-spring/web";
+// import { useTransition, animated } from "react-spring";
 
 function Navigation() {
   const [showMenu, setShowMenu] = useState(false);
 
-  let menu;
-  let maskMenu;
+  const maskTransitions = useTransition(showMenu, {
+    from: { position: "absolute", opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
 
-  if (showMenu) {
-    menu = (
-      <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
-        The menu
-      </div>
-    );
+  const menuTransitions = useTransition(showMenu, {
+    from: { opacity: 0, transform: "translateX(-100%)" },
+    enter: { opacity: 1, transform: "translateX(0%)" },
+    leave: { opacity: 0, transform: "translateX(-100%)" },
+  });
 
-    maskMenu = (
-      <div
-        className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
-        onClick={() => setShowMenu(false)}
-      ></div>
-    );
-  }
   return (
-    <nav className="">
+    <nav>
       <span className="text-xl">
-        <FontAwesomeIcon icon={faBars} onClick={() => setShowMenu(!showMenu)} />
+        <FontAwesomeIcon icon={faBars} onClick={() => setShowMenu(true)} />
       </span>
 
-      {maskMenu}
+      {maskTransitions((style, item) => (
+        item && 
+        <animated.div
+          style={style}
+          className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+          onClick={() => setShowMenu(false)}
+        ></animated.div>
+      ))}
 
-      {menu}
+      {menuTransitions((style, item) => (
+        item && 
+        <animated.div
+          style={style}
+          className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow p-3"
+        >
+          Haha
+        </animated.div>
+      ))}
     </nav>
+    // <nav>
+    //   <span className="text-xl">
+    //     <FontAwesomeIcon icon={faBars} onClick={() => setShowMenu(!showMenu)} />
+    //   </span>
+
+    // {maskTransitions.map(
+    //   ({ item, key, props }) =>
+    //     item && (
+    //       <animated.div
+    //         key={key}
+    //         style={props}
+    //         className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+    //         onClick={() => setShowMenu(false)}
+    //       ></animated.div>
+    //     )
+    // )}
+
+    // {menuTransitions.map(
+    //   ({ item, key, props }) =>
+    //     item && (
+    //       <animated.div
+    //         key={key}
+    //         style={props}
+    //         className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow p-3"
+    //       >
+    //         {/* <NavigationMenu closeMenu={() => setShowMenu(false)} /> */}
+    //       </animated.div>
+    //     )
+    // )}
+    // </nav>
   );
 }
 
